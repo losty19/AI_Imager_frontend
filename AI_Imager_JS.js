771e2649defd4b35cd3f29db53c11bb7ab5c0a29
Luke Ostrander <luke.ostrander19@gmail.com>
2023-05-22 15:00:02 -0500

    Updated

--- AI_Imager_JS.js
+++ AI_Imager_JS.js
@@ -1,26 +1,20 @@
 // AI_Imager_JS.js
 
 // Function to handle image upload
-function handleImageUpload(event) {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-        const imagePreview = document.getElementById("image-preview");
-        imagePreview.style.backgroundImage = `url(${e.target.result})`;
-    };
-
-    reader.readAsDataURL(file);
+function handleTextInput() {
+    const textInput = document.getElementById("text-input").value;
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = `Text Input: ${textInput}`;
 }
 
 // Function to process image using AI
-function processImage() {
+function processText() {
     // TODO: Implement AI processing logic here
     const resultDiv = document.getElementById("result");
-    resultDiv.textContent = "Image processing in progress...";
+    resultDiv.textContent = "Text processing in progress...";
 }
 
 // Event listeners
-document.getElementById("image-input").addEventListener("change", handleImageUpload);
-document.getElementById("process-btn").addEventListener("click", processImage);
+document.getElementById("text-input").addEventListener("input", handleTextInput);
+document.getElementById("process-btn").addEventListener("click", processText);
 
