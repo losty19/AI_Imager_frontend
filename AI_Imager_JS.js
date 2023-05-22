f7fdaca10465f8fd5547045fe250b911206a52fe
Luke Ostrander <luke.ostrander19@gmail.com>
2023-05-22 17:26:49 -0500

    .

--- AI_Imager_JS.js
+++ AI_Imager_JS.js
@@ -1,3 +1,40 @@
+// JavaScript code for your frontend file
+
+// Event listener for form submission
+document.querySelector('form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+  
+    const textInput = document.querySelector('#text-input').value;
+  
+    try {
+      const response = await fetch('/generate-image', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `text_input=${encodeURIComponent(textInput)}`
+      });
+  
+      if (response.ok) {
+        const data = await response.json();
+        const imageUrl = data.image_url;
+  
+        // Display the generated image on the webpage
+        const imageElement = document.createElement('img');
+        imageElement.src = imageUrl;
+        document.body.appendChild(imageElement);
+      } else {
+        console.error('Image generation failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+});
+  
+
+
+/*   *** BELOW IS OLD CODE ***
+
 // AI_Imager_JS.js
 
 // Function to handle image upload
@@ -8,13 +45,40 @@ function handleTextInput() {
 }
 
 // Function to process image using AI
-function processText() {
+async function processText() {
     // TODO: Implement AI processing logic here
+
     const resultDiv = document.getElementById("result");
-    resultDiv.textContent = "Text processing in progress...";
+
+    const apiKey = "";
+    const prompt = 'Image based on text: ${textInput}';
+    
+    const response = await fetch("https://api.openai.com/v1/images", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": 'Bearer ${apiKey}'
+        },
+        body: JSON,stringify({
+            prompt: prompt
+        })
+    });
+
+    const responseData = await response.json();
+    const imageSrc = responseData?.images?.[0]?.image?.url;
+
+    if (imageSrc) {
+        resultDiv.innerHTML = `<img src="${imageSrc}" alt="Generated Image">`;
+    } else {
+        resultDiv.textContent = "Image generation failed.";
+    }
+
+    
+    //resultDiv.textContent = "Text processing in progress...";
 }
 
 // Event listeners
 document.getElementById("text-input").addEventListener("input", handleTextInput);
 document.getElementById("process-btn").addEventListener("click", processText);
 
+*/
