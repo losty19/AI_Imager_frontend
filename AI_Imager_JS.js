fd990bff91affc4ecee72ad484fc2a1b45e0d06b
Luke Ostrander <luke.ostrander19@gmail.com>
2023-05-22 13:41:36 -0500

    Starter Code

--- AI_Imager_JS.js
+++ AI_Imager_JS.js
@@ -1,1 +1,26 @@
+// AI_Imager_JS.js
+
+// Function to handle image upload
+function handleImageUpload(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+        const imagePreview = document.getElementById("image-preview");
+        imagePreview.style.backgroundImage = `url(${e.target.result})`;
+    };
+
+    reader.readAsDataURL(file);
+}
+
+// Function to process image using AI
+function processImage() {
+    // TODO: Implement AI processing logic here
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = "Image processing in progress...";
+}
+
+// Event listeners
+document.getElementById("image-input").addEventListener("change", handleImageUpload);
+document.getElementById("process-btn").addEventListener("click", processImage);
 
